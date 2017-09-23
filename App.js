8c9e0eb3ea99cd67830041434b49e6ab42b3ab16
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2017-09-23 14:51:52 +0200

    Screens SignUp/SignIn

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import { SignedOut } from "./navigation/Rooter";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return <SignedOut />;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
